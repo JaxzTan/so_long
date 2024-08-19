fe375ff4624269cbe04a9ad7e3b51d1f8418bdd7
Cheng Soo Tan <chtan@u90z05s01.42kl.edu.my>
2024-08-19 16:21:11 +0800

    add libft

---
+++ libft/src/ft_strmapi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chtan <chtan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/13 11:27:43 by chtan             #+#    #+#             */
+/*   Updated: 2024/08/09 18:02:34 by chtan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*result;
+	size_t	i;
+
+	if (!s || !f)
+		return (NULL);
+	i = ft_strlen(s);
+	result = (char *)malloc(i + 1);
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (*(s + i))
+	{
+		*(result + i) = f(i, *(s + i));
+		i++;
+	}
+	*(result + i) = '\0';
+	return (result);
+}
